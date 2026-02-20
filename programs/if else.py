30726d2ee1b348d9bde5f3301be86be1b983b4fa
saksham123e <sakshamarora1275@gmail.com>
2026-02-20 18:05:16 +0530

    add folder

---
+++ programs/if else.py
@@ -0,0 +1,10 @@
+a = int(input("enter your age"))
+
+if(a>18):
+    print("you are allowed in a concert", a)
+
+elif(a<0):
+    print("no negative answer allowed", a)
+
+else:
+    print("you are not allowed in concert", a)
---
+++ programs/if else2.py
@@ -0,0 +1,10 @@
+num = int(input("enter the number"))
+
+if ( num % 2 == 0 ):
+    print("number is even", num)
+
+elif( num % 2 != 0):
+    print("number is odd", num)
+
+else:
+    print("number is negative", num)
