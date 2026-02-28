e484f763f4c2acc11c380f81e5a268221ee9be49
saksham123e <sakshamarora1275@gmail.com>
2026-02-28 17:51:43 +0530

    add ascending bubble sort

---
+++ programs/ascending bubble sort.py
@@ -0,0 +1,10 @@
+arr = [7, 3, 9, 1, 5]
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+print("final array:", arr)
