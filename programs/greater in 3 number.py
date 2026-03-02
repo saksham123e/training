0df0cb52aab76bbe8086abc2651bc800251dbb9c
saksham123e <sakshamarora1275@gmail.com>
2026-03-02 17:54:38 +0530

    add greater in 3 number

---
+++ programs/greater in 3 number.py
@@ -0,0 +1,12 @@
+num1 = int(input("enter number1:"))
+num2 = int(input("enter number2:"))
+num3 = int(input("enter number3:"))
+
+if num1 > num2 and num1 > num3:
+    print("num1 is greater number", num1)
+
+elif num2 > num1 and num2 > num3:
+    print("num2 is greater number", num2)
+
+else:
+    print("num3 is greater number", num3) 
