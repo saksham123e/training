71d1cabe06a89d9d8b25b2979025e95f07d5b482
saksham123e <sakshamarora1275@gmail.com>
2026-03-06 17:34:12 +0530

    add asscendingsort.py

---
+++ programs/asscendingselectionsort.py
@@ -0,0 +1,14 @@
+arr = [29, 10, 14, 37, 13]
+
+n = len(arr)
+
+for i in range(n):
+    min_index = i
+
+    for j in range(i+1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print(arr)
