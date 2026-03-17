e9e604d0c8cfa57e0d89df4df66abd5132b4fe99
saksham123e <sakshamarora1275@gmail.com>
2026-03-17 17:51:56 +0530

    add sizeofstack

--- programs/arrayinbuiltday2.py
+++ programs/arrayinbuiltday2.py
@@ -3,3 +3,40 @@ arr = [1,5,3]
 print(max(arr))
 
 
+arr = [1,2,3]
+arr.remove(2)
+print(arr)
+
+
+arr = [1,2,3]
+arr.pop()
+
+
+
+arr = [1,2,3]
+arr.clear()
+
+
+
+arr = [10,20,30]
+print(arr.index(20))
+
+
+arr = [10, 20, 30, 40]
+
+del arr[1] #this is delete function
+
+print(arr)
+
+
+
+arr = [10,20,30]
+
+print(arr[-1]) #negative silicing
+
+
+arr = [4,1,3]
+
+b = sorted(arr)
+
+print(b)
