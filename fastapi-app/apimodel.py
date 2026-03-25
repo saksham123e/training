94c833f84c22a2670549d8d3f203ed1b278b822c
saksham123e <sakshamarora1275@gmail.com>
2026-03-25 18:28:00 +0530

    add apimodel

---
+++ fastapi-app/apimodel.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.post("/user")
+def create_user(user: User):
+    return {
+        "name": user.name,
+        "age": user.age
+    }
