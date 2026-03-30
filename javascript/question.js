16929dc628fbe5edd70d870c1ff4563fcb30530d
saksham123e <sakshamarora1275@gmail.com>
2026-03-30 18:22:48 +0530

    add practices.js

--- javascript/question.js
+++ javascript/question.js
@@ -1,3 +1,39 @@
+// console.log("Hello World");
+
+/*
+let name = "Saksham";
+let age = 20;
+
+console.log(name);
+console.log(age);
+*/
+
+/*
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+*/
+
+/*
+let num = 7;
+
+if (num % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+*/
+
+
+
+/*
 function add(a, b) {
     return a + b;
 }
+
+console.log(add(2, 3));
+
+*/
+
+
