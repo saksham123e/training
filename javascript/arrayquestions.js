88f69e88b7c729dd7598ec060afdfa086f47fe69
saksham123e <sakshamarora1275@gmail.com>
2026-03-31 18:20:40 +0530

    add arrayquestion

---
+++ javascript/arrayquestions.js
@@ -0,0 +1,50 @@
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+
+
+/*
+let arr = [10, 20, 30];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+}
+
+console.log(sum);
+*/
+
+
+
+/*
+let arr = [5, 2, 9, 1];
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
+
+console.log(max);
+*/
+
+
+/*
+let arr = [1, 2, 3, 4, 6];
+let count = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        count++;
+    }
+}
+
+console.log(count);
+*/
+
+
+
