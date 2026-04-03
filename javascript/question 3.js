b34f17992276aad30ceea29036524c1ae16ca1bc
saksham123e <sakshamarora1275@gmail.com>
2026-04-03 17:03:12 +0530

    add question3

---
+++ javascript/question 3.js
@@ -0,0 +1,17 @@
+let myPromise = new Promise((resolve, reject) => {
+    let success = true;
+
+    if (success) {
+        resolve("Task completed");
+    } else {
+        reject("Task failed");
+    }
+});
+
+myPromise
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
