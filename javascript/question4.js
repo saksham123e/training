48cf3e3d3322d415641480dc843f35317081db76
saksham123e <sakshamarora1275@gmail.com>
2026-04-03 17:05:31 +0530

    add question3

---
+++ javascript/question4.js
@@ -0,0 +1,15 @@
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Data fetched successfully");
+        }, 2000);
+    });
+}
+
+fetchData()
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
