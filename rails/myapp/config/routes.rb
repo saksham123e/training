206e179c01155f5d645c739e6772798ce37c36f6
saksham123e <sakshamarora1275@gmail.com>
2026-04-15 18:22:16 +0530

    add authors

--- rails/myapp/config/routes.rb
+++ rails/myapp/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get "authors/index"
+  get "authors/show"
   get "index/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
+  get "authors/index"
+  get "authors/show"
     root "index#index"
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,7 +15,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
+  get "about", to: "pages#about"
   # Defines the root path route ("/")
   # root "posts#index"
 end
