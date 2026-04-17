32e64eb56791509d6f067fb6aef5acd77e84511b
saksham123e <sakshamarora1275@gmail.com>
2026-04-17 18:14:02 +0530

    add raile.work

--- rails/myapp/db/schema.rb
+++ rails/myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_16_124717) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_17_120918) do
   create_table "authors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
