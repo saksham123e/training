4ed71f45f12d4359f865d4e76c74382641ac2400
saksham123e <sakshamarora1275@gmail.com>
2026-04-21 16:36:34 +0530

    add rails practice

--- rails/myapp/db/schema.rb
+++ rails/myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_17_120918) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_21_063519) do
   create_table "authors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_17_120918) do
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name"
+    t.integer "price"
     t.datetime "updated_at", null: false
   end
 
