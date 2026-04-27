c6fc318840980132b2f4d45e066e009a6ee52ff2
saksham123e <sakshamarora1275@gmail.com>
2026-04-27 18:41:02 +0530

    add routers for controller

--- rails/bookapp/config/routes.rb
+++ rails/bookapp/config/routes.rb
@@ -2,3 +2,10 @@ Rails.application.routes.draw do
   resources :books
  get "books/test_data", to: "books#test_data"
 end
+Rails.application.routes.draw do
+  resources :books
+
+  # 👇 YAHAN ADD KAR
+  get "books/full_info", to: "books#full_info"
+  get "books/search", to: "books#search"
+end
