282f9169dfd794172c383cf4f34d5a051b7d77a8
saksham123e <sakshamarora1275@gmail.com>
2026-04-28 13:04:48 +0530

    removed bookapp

--- rails/bookapp/app/models/profile.rb
+++
@@ -1,4 +0,0 @@
-class Profile < ApplicationRecord
-  belongs_to :user
-  has_one :book, through: :user
-end
--- rails/bookapp/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  has_many :reviews
-  has_many :books
-  has_one :profile
-  has_many :reviewed_books, through: :reviews, source: :book
-end
