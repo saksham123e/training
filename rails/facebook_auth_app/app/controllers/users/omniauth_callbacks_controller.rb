867b8ff8c94045cce0d21eda9583e9b239d043f4
saksham123e <sakshamarora1275@gmail.com>
2026-05-14 18:54:33 +0530

    added project

--- rails/facebook_auth_app/app/controllers/users/omniauth_callbacks_controller.rb
+++ rails/facebook_auth_app/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if user.persisted?
-      sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      flash[:notice] = "Facebook login successful"
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+  end
+
+  def failure
+    redirect_to root_path
   end
 end
