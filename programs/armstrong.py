afb617f916c11750910e248387e9a99fbb8249c2
saksham123e <sakshamarora1275@gmail.com>
2026-05-21 18:13:42 +0530

    added database

--- programs/armstrong.py
+++ programs/armstrong.py
@@ -1,14 +1,10 @@
-n = int(input("enter your number"))
-temp = n
-power = len(str(n))
-sum = 0
-while temp > 0:
-    digit = temp % 10
-    sum = sum + digit ** power
-    temp = temp // 10
-
-if sum == n:
-    print("n is armstrong:", n)
-
-else:
-    print("n is not armstrong:", n)
+arr = [10, 5, 20, 8, 15]
+max = 0
+max2 = 0
+for num in arr:
+    if num > max and num > max2:
+        max2 = max
+        max = num
+    elif num >max2 and num != max:
+        max2 = num
+print("The second largest number is:", max2)
