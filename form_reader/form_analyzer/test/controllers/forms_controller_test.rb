5bc3c801c38d01d9bc8b08725724f53f83cacb75
saksham123e <sakshamarora1275@gmail.com>
2026-05-26 17:36:19 +0530

    added form_anylyzer

---
+++ form_reader/form_analyzer/test/controllers/forms_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class FormsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get forms_index_url
+    assert_response :success
+  end
+
+  test "should get analyze" do
+    get forms_analyze_url
+    assert_response :success
+  end
+end
