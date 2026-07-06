963fd2c4e86f8134f8fb4b486f9e604bcb6b34be
saksham123e <sakshamarora1275@gmail.com>
2026-07-06 17:40:16 +0530

    added updated files

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,58 +1,21 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import StudentCard from "./components/StudentCard";
-import EmployeeCard from "./components/EmployeeCard";
-import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-      <Navbar />
+    <div>
 
-      <Hero
-        name="Saksham Arora"
-        role="Full Stack Developer"
-        description="Currently learning React fundamentals to become better in Next.js."
-      />
+      <h1>Counter App</h1>
 
-      <About />
-      <Skills />
-      <Projects />
+      <h2>{count}</h2>
 
-      <h2>Student Details</h2>
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
 
-      <StudentCard name="Saksham Arora" course="B.Tech CSE" age="22" />
-      <StudentCard name="Rahul Sharma" course="BCA" age="21" />
-      <StudentCard name="Priya Singh" course="MCA" age="23" />
-
-      <h2>Employee Details</h2>
-
-      <EmployeeCard
-        name="Aman"
-        designation="Backend Developer"
-        salary="12 LPA"
-        experience="3 Years"
-      />
-
-      <EmployeeCard
-        name="Priya"
-        designation="Frontend Developer"
-        salary="10 LPA"
-        experience="2 Years"
-      />
-
-      <EmployeeCard
-        name="Rohit"
-        designation="UI Designer"
-        salary="11 LPA"
-        experience="4 Years"
-      />
-
-      <Footer />
-    </>
+    </div>
   );
 }
 
